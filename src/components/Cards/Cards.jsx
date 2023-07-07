cd47424328e497bdc69e01fc140c59be9d4a6339
Omarx32 <rmarin.0812@gmail.com>
2023-07-07 18:28:35 -0430

    Agregando proyecto

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,26 @@
+import style from './Cards.module.css'
+import Card from './Card/Card'
+
+const Cards = ({ characters, onClose }) => {
+    
+    return <>
+        <div className={style.container}>{
+            characters.map(card => 
+                <Card
+                    key={card.id}
+                    name={card.name}
+                    status={card.status}
+                    species={card.species}
+                    gender={card.gender}
+                    origin={card.origin.name}
+                    image={card.image}
+                    onClose={onClose}
+                    id={card.id}
+                ></Card>
+            )}
+        </div>
+    
+    </>
+}; 
+
+export default Cards
